c722b51d1c103266576157227e115f01d57678e9
Erica <ericaesnider@gmail.com>
2017-06-18 16:27:32 -0500

    add favicon

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var app = express();
 
 app.use(express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/bower_components'));
 app.use(bodyParser.json());
+app.use(favicon(__dirname + '/client/static/img/favicon.ico'))
 
 require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
