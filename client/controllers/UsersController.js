a3431c4a8ea0255204851bcb13bb2dd131e05d1f
Erica <ericaesnider@gmail.com>
2017-06-19 10:12:14 -0500

    provide error when submitting blank registration form

--- client/controllers/UsersController.js
+++ client/controllers/UsersController.js
@@ -18,27 +18,33 @@ app.controller('UsersController', function(userFactory, $location, $routeParams,
   }
   self.create = function(newUser){
     self.registrationErrors = [];
-    if (newUser.password != newUser.password_confirmation) {
-      self.registrationErrors = ['Passwords do not match.'];
+    self.loginErrors = [];
+    if (newUser) {
+      if (newUser.password != newUser.password_confirmation) {
+        self.registrationErrors = ['Passwords do not match.'];
+      } else {
+        userFactory.create(newUser, function(res){
+          self.newUser = {};
+          if (res.data.errors || res.data.code == 1100) {
+            if (res.data.code == 11000) {
+              self.registrationErrors.push('Email is already registered.');
+            }
+            for(key in res.data.errors){
+              self.registrationErrors.push(res.data.errors[key].message);
+            }
+          } else {
+            $cookies.put('userId', res.data._id);
+            $location.url('/');
+          }
+        })
+      }
     } else {
-      userFactory.create(newUser, function(res){
-        self.newUser = {};
-        if (res.data.errors || res.data.code == 1100) {
-          if (res.data.code == 11000) {
-            self.registrationErrors.push('Email is already registered.');
-          }
-          for(key in res.data.errors){
-            self.registrationErrors.push(res.data.errors[key].message);
-          }
-        } else {
-          $cookies.put('userId', res.data._id);
-          $location.url('/');
-        }
-      })
+      self.registrationErrors.push('Registration cannot be blank.')
     }
   }
   self.login = function(checkUser){
     self.loginErrors = [];
+    self.registrationErrors = [];
     userFactory.login(checkUser, function(res){
       self.checkUser = {};
       if (res.data.errors) {
